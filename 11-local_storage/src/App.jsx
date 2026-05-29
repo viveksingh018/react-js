163e66c497161545950166e1c6cdbab85d4a4574
Vivek Singh <vivekkumarhcc245@gmail.com>
2026-05-29 11:05:45 +0530

    understanding localStorage

---
+++ 11-local_storage/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+
+  const user = JSON.parse(localStorage.getItem('user'))
+  console.log(user)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
